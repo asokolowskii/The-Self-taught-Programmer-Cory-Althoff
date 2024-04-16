faeb0cb624fea70927785d4218f32e63aaf5be4c
Adam Sokołowski <161360927+asokolowskii@users.noreply.github.com>
2024-04-16 13:42:45 +0200

    First Commit
    16.04.2024

---
+++ Task 1.py
@@ -0,0 +1,7 @@
+"""
+Try to print something other than Hello, World!.
+"""
+
+print("Welcome to my first repository")
+print("This is a simple Python programme")
+print("The code is written in Python 3")
