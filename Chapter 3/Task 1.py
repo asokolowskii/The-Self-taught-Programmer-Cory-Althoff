01d4a96709385e9530ab021b2df3bcd5aaec114a
Adam Sokołowski <161360927+asokolowskii@users.noreply.github.com>
2024-04-16 22:11:47 +0200

    Project Edit 1

---
+++ Chapter 3/Task 1.py
@@ -0,0 +1,7 @@
+"""
+Print three different strings.
+"""
+
+print("Hello World!")
+print("Welcome to my repository!")
+print("This is my first programming book")
---
+++ Chapter 3/Task 2.py
@@ -0,0 +1,10 @@
+"""
+Write a program that prints a message if a variable is less than 10, and different message
+if the variable is greater than or equal to 10.
+"""
+
+num = 15
+if num < 10:
+    print("Your number is lower than 10!")
+else:
+    print("Your number is greater than or equal to 10!")
